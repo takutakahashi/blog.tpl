19897e2e13fe554c4a24602735df81160c0fc32d
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2021-03-21 01:24:06 +0900

    add images

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ sed -i "s/BLOG_TITLE/$BLOG_TITLE/" config.yml
 sed -i "s/BLOG_URL/$BLOG_URL/" config.yml
 sed -i "s/BLOG_GA/$BLOG_GA/" config.yml
 git clone $BLOG_THEME_URL themes/thistheme
-exec notion-tpl --table-id $TABLE_ID --token $TOKEN --export-path /src/content/posts --template /etc/notion-tpl/hugo.md.tpl --cmd hugo
+exec notion-tpl --table-id $TABLE_ID --token $TOKEN --export-path /src/content/posts --image-path /src/images --template /etc/notion-tpl/hugo.md.tpl --cmd hugo
