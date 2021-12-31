8f9bd4cce9e5bfa7302b1b58fb74e3bf4b1457eb
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2022-01-01 00:23:17 +0900

    add once

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ if [[ "$BLOG_THEME_BRANCH" = "" ]]; then
   BLOG_THEME_BRANCH="master"
 fi
 git clone $BLOG_THEME_URL themes/thistheme -b "$BLOG_THEME_BRANCH" || git clone $BLOG_THEME_URL themes/thistheme -b master || git clone $BLOG_THEME_URL themes/thistheme -b main
-exec notion-tpl --table-id $TABLE_ID --token $TOKEN --export-path /src/content/posts --image-path /src/images --template /etc/notion-tpl/hugo.md.tpl --cmd hugo
+exec notion-tpl --table-id $TABLE_ID --token $TOKEN --export-path /src/content/posts --image-path /src/images --template /etc/notion-tpl/hugo.md.tpl --cmd hugo $ONCE
